1daa0e43f4f4a2cffee1b48eafbf4f5647ca8305
eternal-fy <906542557@qq.com>
2022-03-23 23:06:59 +0800

    fix

--- front/src/components/Bar.vue
+++ front/src/components/Bar.vue
@@ -1,7 +1,14 @@
 <template>
   <div id="bar">
     <div class="context">
-      <div v-for="(num,index) in 3" :key="index">{{num}}</div>
+      <div class="loginArea">
+        <a href="https://www.baidu.com" target="_blank">
+          <div>
+            个人中心
+            <img width="50" height="50" src="../assets/personalCenter.svg"/>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </template>
@@ -13,19 +20,29 @@ export default {
 </script>
 
 <style scoped>
-#bar{
-  background-color: black;
+#bar {
+  background-color: #6b6464;
   opacity: 0.5;
   position: fixed;
   width: 100%;
+  height: 50px;
   padding: 20px 50px;
   z-index: 20;
   line-height: 50px;
+  overflow: hidden;
   border-bottom: 1px solid rgba(0, 0, 0, 0.11);
 }
-.context{
+
+.context {
   display: flex;
   flex-direction: row;
+}
+
+.loginArea {
+  width: 500px;
+  line-height: 50px;
+  font-max-size: 16px;
+
 
 }
 </style>
