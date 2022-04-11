f4593cd0fc12dc6ccc88bd813f7a00394b59a77e
eternal-fy <906542557@qq.com>
2022-04-11 22:25:53 +0800

    添加跨域请求头

--- front/src/main.js
+++ front/src/main.js
@@ -11,6 +11,7 @@ const app=createApp(App);
 //默认使用axios
 //更改axios默认路径，改为 /api 原因是要在vue.config.js里面配置
 axios.defaults.baseURL='http://eternalfy.site:9999'
+axios.defaults.headers['Access-Control-Allow-Origin'] = 'http://eternalfy.site:9999'
 app.config.globalProperties.$http = axios
 app.use(vueaxios,axios)
 app.use(router)
