1f00a4b31790bc11e14ec3d418d5826690fc93ba
eternal-fy <906542557@qq.com>
2022-04-11 17:20:12 +0800

    代理配置fix

--- front/vue.config.js
+++ front/vue.config.js
@@ -7,7 +7,7 @@ module.exports = {
                 ws:true,//代理websocked
                 changeOrigin:true,//虚拟的站点需要更管origin
                 pathRewrite:{
-                    '^/api':''//重写路径
+                    '^/api':'api'//重写路径
                 }
             }
         },
