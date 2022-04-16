91d8a41638da2f12e66b37fc2d62848d89bff484
eternal-fy <906542557@qq.com>
2022-04-16 14:50:38 +0800

    后端测试内容

--- backend/controllers/entryController.go
+++ backend/controllers/entryController.go
@@ -1,10 +1,8 @@
 package controllers
 
 import (
-	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
 	"net/http"
-	"net/url"
 )
 
 type EntryController struct {
@@ -13,16 +11,19 @@ type EntryController struct {
 
 //QQ联合登陆验证
 func (c *EntryController) Auth() {
-	params := url.Values{}
-	appId := "102004026"
-	params.Add("response_type", "code")
-	params.Add("client_id", appId)
-	params.Add("state", "test")
-	redirectURI := "/login"
-	str := fmt.Sprintf("%s&redirect_uri=%s", params.Encode(), redirectURI)
-	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/authorize", str)
-	println(loginURL)
-	c.Redirect(loginURL, http.StatusFound)
+	uri := c.Ctx.Request.RequestURI
+	println(uri)
+	c.Redirect("http://www.eternalfy.site/main-page/main-context/index", http.StatusFound)
+	/*	params := url.Values{}
+		appId := "102004026"
+		params.Add("response_type", "code")
+		params.Add("client_id", appId)
+		params.Add("state", "test")
+		redirectURI := "/login"
+		str := fmt.Sprintf("%s&redirect_uri=%s", params.Encode(), redirectURI)
+		loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/authorize", str)
+		println(loginURL)*/
+	//c.Redirect(loginURL, http.StatusFound)
 }
 
 func (c *EntryController) Login() {
