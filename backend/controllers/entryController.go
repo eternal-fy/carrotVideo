95f93a9b2f604a163071e1d9eb08e486d68cdae7
eternal-fy <906542557@qq.com>
2022-04-24 11:29:57 +0800

    添加三方登陆获取信息fix

--- backend/controllers/entryController.go
+++ backend/controllers/entryController.go
@@ -16,35 +16,34 @@ type EntryController struct {
 func (c *EntryController) Auth() {
 	params := url.Values{}
 	AppId := "102005140"
-	params.Add("response_type", "token")
+	params.Add("response_type", "code")
 	params.Add("client_id", AppId)
 	params.Add("state", "test")
-	redirectURI := "http%3A%2F%2Feternalfy.site%2Fapi%2Fentry%2Fgettoken"
+	redirectURI := "http%3A%2F%2Feternalfy.site%2Fapi%2Fentry%2Fgetcode"
 	str := fmt.Sprintf("%s&redirect_uri=%s", params.Encode(), redirectURI)
 	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/authorize", str)
 	c.Ctx.ResponseWriter.Write([]byte(loginURL))
 }
 
-func (c *EntryController) GetToken() {
-	token := c.Ctx.Input.Query("access_token")
-	ctx := c.Ctx
-	println(ctx)
-	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/me?access_token=", token)
-
+func (c *EntryController) GetCode() {
+	code := c.Ctx.Input.Query("code")
+	params := url.Values{}
+	AppId := "102005140"
+	params.Add("response_type", "code")
+	params.Add("client_id", AppId)
+	params.Add("client_secret", AppId)
+	params.Add("code", code)
+	redirectURI := "http%3A%2F%2Feternalfy.site%2Fapi%2Fentry%2Fgettoken"
+	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code", redirectURI)
 	response, _ := http.Get(loginURL)
 	defer response.Body.Close()
-
 	bs, _ := ioutil.ReadAll(response.Body)
 	body := string(bs)
 	println(body)
-	/*resultMap := convertToMap(body)
-
-	info := &PrivateInfo{}
-	info.AccessToken = resultMap["access_token"]
-	info.RefreshToken = resultMap["refresh_token"]
-	info.ExpiresIn = resultMap["expires_in"]
-
-	GetOpenId(info, w)*/
+}
+func (c *EntryController) GetToken() {
+	token := c.Ctx.Input.Query("access_token")
+	println(token)
 
 }
 
