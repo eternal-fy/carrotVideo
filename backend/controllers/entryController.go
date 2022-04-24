633c86f7e1625fe9b185886603b56f67a4423725
eternal-fy <906542557@qq.com>
2022-04-24 20:57:21 +0800

    fix

--- backend/controllers/entryController.go
+++ backend/controllers/entryController.go
@@ -33,6 +33,7 @@ func (c *EntryController) GetCode() {
 	params.Add("client_id", AppId)
 	params.Add("client_secret", Secret)
 	params.Add("code", code)
+	params.Add("fmt", "json")
 	redirectURI := "http%3A%2F%2Feternalfy.site%2Fapi%2Fentry%2Fgettoken"
 	loginURL := fmt.Sprintf("%s&%s&redirect_uri=%s", "https://graph.qq.com/oauth2.0/token?grant_type=authorization_code", params.Encode(), redirectURI)
 	response, _ := http.Get(loginURL)
