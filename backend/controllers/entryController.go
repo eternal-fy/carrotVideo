96c1ce11b753211839cedccb25816943976874e3
eternal-fy <906542557@qq.com>
2022-04-24 11:07:30 +0800

    添加三方登陆获取信息

--- backend/controllers/entryController.go
+++ backend/controllers/entryController.go
@@ -3,6 +3,8 @@ package controllers
 import (
 	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
+	"io/ioutil"
+	"net/http"
 	"net/url"
 )
 
@@ -14,7 +16,7 @@ type EntryController struct {
 func (c *EntryController) Auth() {
 	params := url.Values{}
 	AppId := "102005140"
-	params.Add("response_type", "code")
+	params.Add("response_type", "token")
 	params.Add("client_id", AppId)
 	params.Add("state", "test")
 	redirectURI := "http%3A%2F%2Feternalfy.site%2Fapi%2Fentry%2Fgettoken"
@@ -24,36 +26,26 @@ func (c *EntryController) Auth() {
 }
 
 func (c *EntryController) GetToken() {
-	//code := c.Ctx.Input.Query("access_token")
+	token := c.Ctx.Input.Query("access_token")
 	ctx := c.Ctx
 	println(ctx)
+	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/me?access_token=", token)
 
-	/*params := url.Values{}
-	params.Add("grant_type", "authorization_code")
-	params.Add("client_id", AppId)
-	params.Add("client_secret", AppKey)
-	params.Add("code", code)
-	str := fmt.Sprintf("%s&redirect_uri=%s", params.Encode(), redirectURI)
-	loginURL := fmt.Sprintf("%s?%s", "https://graph.qq.com/oauth2.0/token", str)
-
-	response, err := http.Get(loginURL)
-	if err != nil {
-		w.Write([]byte(err.Error()))
-	}
+	response, _ := http.Get(loginURL)
 	defer response.Body.Close()
 
 	bs, _ := ioutil.ReadAll(response.Body)
 	body := string(bs)
-
-	resultMap := convertToMap(body)
+	println(body)
+	/*resultMap := convertToMap(body)
 
 	info := &PrivateInfo{}
 	info.AccessToken = resultMap["access_token"]
 	info.RefreshToken = resultMap["refresh_token"]
 	info.ExpiresIn = resultMap["expires_in"]
 
-	GetOpenId(info, w)
-	*/
+	GetOpenId(info, w)*/
+
 }
 
 func (c *EntryController) Login() {
